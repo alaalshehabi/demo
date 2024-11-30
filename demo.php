33592b5b7c47985f8c61093a8da1edb28cb2fa5c
alaalshehabi <202100334@stu.uob.edu.bh>
2024-11-30 21:46:34 +0300

    change

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 https://github.com/alaalshehabi/demo.
 <?php
-//Task 1: Fetch student data from the API
+//First Task: Fetch student data from the API
 
 // Define the API endpoint URL to fetch student data
 $url = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
